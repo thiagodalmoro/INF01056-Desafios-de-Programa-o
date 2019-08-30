2f1076157512d744bc51f1a4b71b9f099ccdd156
thiagodalmoro <thiagordalmoro@gmail.com>
2019-08-30 10:06:13 -0300

    Update desafios01.py
    exercicioparte1

--- desafios01.py
+++ desafios01.py
@@ -7,8 +7,22 @@ import math
 import  numpy
 
 
+M = {
+    "key",
+    "value",
+    "quantity"
+}
 
-def GotoMarket()
-    trips = int(input("Enter number of Trips GotoMarket: "))
-    print  (trips)
-    
+
+def main ():
+    #N number os trips
+    N = int(input("Enter number of Trips GotoMarket: "))
+    print  (N)
+
+
+    M.quantity=int(input("Enter number of quantity list disponible products: "))
+    print   (M.quantity)
+
+
+    #
+    # for x in

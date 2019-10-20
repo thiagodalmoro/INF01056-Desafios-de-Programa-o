a09d2034be9416e55fbd38e9e4672a777642f23f
thiagodalmoro <thiagordalmoro@gmail.com>
2019-10-20 17:46:21 -0300

    aula05

---
+++ aula_05/credo.py
@@ -0,0 +1,21 @@
+#autor: Thiago
+
+# rodar dijkstra para encontrar a matriz de alcancabilidade
+# algoritmo de floyd washton
+
+
+# entrada d
+
+entrada = []
+
+N = int(input())
+
+entrada = input().split()
+
+    for i in range(len(entrada)):
+        entrada[i] = int(entrada[i])
+
+    # print (entrada)
+
+    cv = 0
+    trab = 0
